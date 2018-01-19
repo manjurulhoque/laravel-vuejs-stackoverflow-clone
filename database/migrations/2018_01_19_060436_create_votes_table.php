f1f1dd3230c58c3706bf4a6d4cf417652d617831
manjurulhoque <rumimanzurulhoque@gmail.com>
2018-01-19 12:07:47 +0600

    vote model, controller and migrations

---
+++ database/migrations/2018_01_19_060436_create_votes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('votes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->boolean('upvote');
+            $table->boolean('downvote');
+            $table->boolean('favorite');
+            $table->integer('user_id');
+            $table->integer('question_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('votes');
+    }
+}
