cf670aadd5890ab4a026ceefd58cbe26eaf4a1f5
manjurulhoque <rumimanzurulhoque@gmail.com>
2018-01-22 15:31:31 +0600

    user and profile relation

--- app/Profile.php
+++ app/Profile.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/User.php
+++ app/User.php
@@ -36,4 +36,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Reply::class);
     }
+
+    public function profile()
+    {
+        return $this->hasOne(Profile::class);
+    }
 }
