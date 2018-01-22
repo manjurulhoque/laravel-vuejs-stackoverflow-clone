ddf472a17d95e41a58013202595c51499d8e5bec
manjurulhoque <rumimanzurulhoque@gmail.com>
2018-01-22 15:29:41 +0600

    profile model, controller and migrate

---
+++ database/migrations/2018_01_22_092214_create_profiles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('image')->default(null);
+            $table->string('first_name')->default(null);
+            $table->string('last_name')->default(null);
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
