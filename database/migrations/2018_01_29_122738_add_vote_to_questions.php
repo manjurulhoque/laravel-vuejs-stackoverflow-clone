fdca309e0cbc9d3006d4e5cb119d42b88f9a98c6
manjurulhoque <rumimanzurulhoque@gmail.com>
2018-01-29 18:31:23 +0600

    vote column add to questions table

---
+++ database/migrations/2018_01_29_122738_add_vote_to_questions.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddVoteToQuestions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            $table->integer('vote');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            //
+        });
+    }
+}
